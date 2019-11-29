e6cd8a8d2a8a58535726aea23272fb1693fa5c82
danishjames <danishjames@gmail.com>
2019-11-28 21:03:35 -0500

    Scraper commit

---
+++ data/auth.py
@@ -0,0 +1,7 @@
+import pylast
+
+API_KEY = 'ac61bbcfdc980992610c5f0cd6e99b61'
+API_SECRET = 'b14728d29db641a2273661412ea5321e'
+username = 'Mithachu'
+password = 'baccano13.com'
+password_hash = pylast.md5(password)
